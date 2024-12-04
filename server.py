bba01d8c9473d7ceb07931330df727818d9b01c8
Eyegff <158478227+Eyegff@users.noreply.github.com>
2024-12-04 00:10:28 -0800

    Update server.py

--- server.py
+++ server.py
@@ -6,8 +6,8 @@ import os
 
 app = Flask(__name__)
 
-ACCESS_TOKEN = os.environ['FACEBOOK_ACCESS_TOKEN']
-VERIFY_TOKEN = os.environ['VERIFY_TOKEN'] 
+ACCESS_TOKEN = os.environ['EAAXP5ykXJfUBOz0IZCbkX8bMyA1GntgYg46Qx1EzCXezGmlNgPsJsvNW9dXx5UEWjR3mjN3ZCZAZC4ePHKUC7TWpRUGj6KNbh7R9eM0fSsXaIdRFPHbHg9EgRkfjsGx7bnnUNvhj6Pcp6eFIG6ZBeBHFVAjZBzC514oariSYKZBrLFZCnIIkbgY4ZCH0ZBIw8YZAn5d4QZDZD']
+VERIFY_TOKEN = os.environ['AAACCCXXX'] 
 
 
 def reply(user_id, msg):
